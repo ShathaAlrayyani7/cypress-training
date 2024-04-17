2be5f5b252b5b1a73ac12f8f2c6528761d96b515
ShathaAlrayyani7 <shatha.alrayyani@optimumpartners.co>
2024-04-17 17:46:57 +0300

    adding another test for sorting data in the table

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,17 +1,9 @@
 import cryptocurrencyPage from "../pages/cryptocurrencyPage";
 import mockData from "../fixtures/mockData.json";
 
-const params = {
-  limit: "100",
-  pageNum: "1",
-  sortBy: "marketCap",
-  direction: "desc",
-  query: "",
-  category: "ft",
-};
 
-describe("template spec", () => {
-  context("with a mock data", () => {
+describe("Testing Forbes's Cryptocurrency Prices Today table", () => {
+  context("with mock data", () => {
     beforeEach(() => {
       const url = `https://fda.forbes.com/v2/tradedAssets?*`;
       cy.fixture("mockData").then((json) => {
@@ -32,26 +24,24 @@ describe("template spec", () => {
 
     it("should change color based on value", () => {
       cy.wait("@getAllData", { timeout: 300_000 }).then((res) => {
-        cy.log("🚀 ~ .then ~ res:", res);
-
         const rowCell1H = cryptocurrencyPage.elements.rowCell1H();
         const rowCell1D = cryptocurrencyPage.elements.rowCell1D();
         const rowCell7D = cryptocurrencyPage.elements.rowCell7D();
 
         rowCell1H
           .eq(0)
-          .should("have.css", "color", "rgb(0, 153, 51)")
-          .should("have.text", "+0.86%");
+          .should("have.text", "+0.86%")
+          .should("have.css", "color", "rgb(0, 153, 51)");
 
         rowCell1D
           .eq(1)
-          .should("have.css", "color", "rgb(220, 0, 0)")
-          .should("have.text", "-7.10%");
+          .should("have.text", "-7.10%")
+          .should("have.css", "color", "rgb(220, 0, 0)");
 
         rowCell7D
           .eq(2)
-          .should("have.css", "color", "rgb(51, 51, 51)")
-          .should("have.text", "0.00%");
+          .should("have.text", "0.00%")
+          .should("have.css", "color", "rgb(51, 51, 51)");
       });
     });
   });
@@ -64,23 +54,80 @@ describe("template spec", () => {
       );
     });
 
-    it("should sort the prices dec", () => {
-      const queryString = cryptocurrencyPage.getQueryString(params);
-      // sortBy: "marketCap"
+    it(
+      "should sort the data based on prices in desc order",
+      { scrollBehavior: false },
+      () => {
+        const arrow = cryptocurrencyPage.elements.priceArrow();
+        const priceHeader = cryptocurrencyPage.elements.priceHeader();
+        const url = `https://fda.forbes.com/v2/tradedAssets?*`;
 
-      // cy.wait("@getAllData", { timeout: 300_000 })
-      //   .its("request.query")
-      //   .then((query) => {
-      //     expect(query.direction).to.equal("desc");
-      //     expect(query.sortBy).to.equal("marketCap");
-      //   });
+        cy.scrollTo(0, 300);
+        // make sure the arrow in not visible yet before the click.
+        arrow.should("not.be.visible");
+        priceHeader.wait(2000).click({ scrollBehavior: false, force: true });
+        arrow.should("be.visible");
 
-      // const dayHeader = cryptocurrencyPage.elements.dayHeader();
-      // dayHeader.scrollIntoView().should('be.visible')
-      // dayHeader.wait(2000).click();
-      // dayHeader.wait(2000).click();
+        cy.intercept(url).as("getAllData");
+        cy.wait("@getAllData", { timeout: 300_000 }).then((data) => {
+          const query = data.request.query;
+          const sortedData = data.response.body.assets;
 
-      // cy.intercept(url, sortedMockData).as("getAllDataByPrice");
-    });
+          expect(query.direction).to.equal("desc");
+          expect(query.sortBy).to.equal("price");
+
+          cryptocurrencyPage
+            .isSorted(sortedData?.slice(0, 20), "price")
+            .should("eq", "Descending");
+
+          // make sure that sorted data is being displayed in the table
+          const rowCellPrice = cryptocurrencyPage.elements.rowCellPrice();
+          for (let i = 0; i < 5; i++) {
+            const cell = rowCellPrice.eq(i).text();
+            expect(sortedData[i].price).to.equal(
+              cryptocurrencyPage.extractNumberFromString(cell)
+            );
+          }
+        });
+      }
+    );
+
+    it(
+      "should sort the data based on prices in asc order",
+      { scrollBehavior: false },
+      () => {
+        const arrow = cryptocurrencyPage.elements.priceArrow();
+        const priceHeader = cryptocurrencyPage.elements.priceHeader();
+        const url = `https://fda.forbes.com/v2/tradedAssets?*`;
+
+        cy.scrollTo(0, 300);
+        // make sure the arrow in not visible yet before the click.
+        arrow.should("not.be.visible");
+        priceHeader.wait(2000).dblclick({ scrollBehavior: false, force: true });
+        arrow.should("be.visible");
+
+        cy.intercept(url).as("getAllData");
+        cy.wait("@getAllData", { timeout: 300_000 }).then((data) => {
+          const query = data?.request?.query;
+          const sortedData = data?.response?.body?.assets;
+
+          expect(query.direction).to.equal("asc");
+          expect(query.sortBy).to.equal("price");
+
+          cryptocurrencyPage
+            .isSorted(sortedData, "price")
+            .should("eq", "Ascending");
+
+          // make sure that sorted data is being displayed in the table
+          const rowCellPrice = cryptocurrencyPage.elements.rowCellPrice();
+          for (let i = 0; i < 5; i++) {
+            const cell = rowCellPrice.eq(i).text();
+            expect(sortedData[i].price).to.equal(
+              cryptocurrencyPage.extractNumberFromString(cell)
+            );
+          }
+        });
+      }
+    );
   });
 });
--- cypress/pages/cryptocurrencyPage.js
+++ cypress/pages/cryptocurrencyPage.js
@@ -1,12 +1,14 @@
 class cryptocurrencyPage {
   elements = {
-    priceHeader: () => cy.get('[data-test=Price]'),
+    priceHeader: () => cy.get("[data-test=Price]"),
+    priceArrow: () => cy.get("[data-test=Price] span:nth-child(2)"),
     hourHeader: () => cy.get("[data-test=1H]"),
     dayHeader: () => cy.get("[data-test=24H]"),
     weekHeader: () => cy.get("[data-test=7D]"),
     rowCell1H: () => cy.get("[data-test=row-cell-1H] span"),
     rowCell1D: () => cy.get("[data-test=row-cell-24H] span"),
     rowCell7D: () => cy.get("[data-test=row-cell-7D] span"),
+    rowCellPrice: () => cy.get("[data-test=row-cell-Price] span"),
   };
 
   clickOnPrice() {
@@ -20,6 +22,51 @@ class cryptocurrencyPage {
     return query;
   }
 
+  isSorted(arr, key) {
+    let ascSorted = true;
+    let descSorted = true;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i][key] > arr[i + 1][key]) {
+        ascSorted = false; // If current element is greater than next, not ascending sorted
+      } else if (arr[i][key] < arr[i + 1][key]) {
+        descSorted = false; // If current element is less than next, not descending sorted
+      }
+    }
+
+    if (ascSorted && descSorted) {
+      // If both flags are still true, the array is neither sorted in ascending nor descending order
+      return "Unsorted";
+    } else if (ascSorted) {
+      return "Ascending";
+    } else if (descSorted) {
+      return "Descending";
+    } else {
+      return "Unsorted";
+    }
+  }
+
+  extractNumberFromString(string) {
+    // Remove "$" sign and abbreviations (B, M, K) from the string
+    const stringWithoutSymbols = string.replace(/\$|[BMK]/g, "");
+
+    // Determine the multiplier based on the abbreviation
+    let multiplier = 1;
+    if (string.includes("B")) {
+      multiplier = 1e9; // Billion
+    } else if (string.includes("M")) {
+      multiplier = 1e6; // Million
+    } else if (string.includes("K")) {
+      multiplier = 1e3; // Thousand
+    } else if (string.includes("T")) {
+      multiplier = 1e12; // Trillion
+    }
+
+    // Convert the string to a number
+    const number = parseFloat(stringWithoutSymbols) * multiplier;
+
+    return number;
+  }
 }
 
 module.exports = new cryptocurrencyPage();
